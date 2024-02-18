914586b017c7060d7a51e8e61ef09c09f5e79620
oscarprdev <vlcoscar8@gmail.com>
2024-02-18 12:20:34 +0100

    Add image domain

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**',
-			},
-		],
+		domains: ['https://pub-17c2fc4e97124c8a93541cac1e9ee69e.r2.dev'],
 	},
 };
 
