a21e9dcea025f3c6c63c8a42fd8ec255f7eb6abb
oscarprdev <vlcoscar8@gmail.com>
2024-02-18 12:14:52 +0100

    Fix images remote patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'pub-17c2fc4e97124c8a93541cac1e9ee69e.r2.dev',
-				port: '',
-				pathname: '/*',
+				hostname: '**',
 			},
 		],
 	},
